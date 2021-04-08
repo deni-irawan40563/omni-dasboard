4c0b99d06320520623ef605748fe61ec836ed9c2
Deni Irawan Nugraha <deni.irawan40563@gmail.com>
2021-04-08 14:28:35 +0700

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+#Omni hiring tes
+
+  Untuk menjalankan projek cukup ketikan di cmd/terminal :
+  npm install
+  lalu
+  npm start
+
+#tentang
+  projek ini saya buat menggunakan React js tanpa menggunakan library lain, menggunakan prinsip Atomic design agar semua komponen bisa digunakan kembali dan semua layoutnya sudah responsive.
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -3,9 +3,6 @@ import React, { Component } from 'react';
 //Import-Style:
 import './style.css';
 
-import Button from '../../Components/Atoms/Button/Button';
-import Form from '../../Components/Atoms/forms/Form';
-
 //Main-Components:
 export default class Error extends Component {
   alett = () => {
@@ -16,9 +13,6 @@ export default class Error extends Component {
       <div id="error-404">
         <h1>404 NOT FOUND</h1>
         <h2>Maaf Kayaknya Pagenya Belum Di Buat deh</h2>
-        <Button title="hallo" onClick={this.alett}/>
-        <hr />
-        <Form addon={true} addonTitle="abd"/>
       </div>
     )
   }
